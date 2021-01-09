2870070a924ff28a0fb47d1aeb5fc6867ca8285a
Robert Mair <48635807+RobertMair@users.noreply.github.com>
2021-01-09 17:11:34 +1000

    Commit 1

--- Program.cs
+++ Program.cs
@@ -1,62 +1,221 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace ExercisesConditionals
 {
     class Program
     {
+        public static ProgramMenu ThisMenu = new ProgramMenu();
+
         static void Main()
         {
-            var exercises = new Dictionary<int, string>
-            {
-                {1, "Number between 1 and 10"},
-                {2, "Maximum of two numbers"},
-                {3, "Orientation of an image"},
-                {4, "Speed camera"}
-            };
 
-            Console.WriteLine("\nExercises - Conditionals:\n\nChoose from the following:\n");
+            // Set program title and menu list.
+            ThisMenu.Title = "\nExercises - Conditionals:\n\nChoose from the following:\n";
+            ThisMenu.Exercises.Add("1", "Number between 1 and 10");
+            ThisMenu.Exercises.Add("2", "Maximum of two numbers");
+            ThisMenu.Exercises.Add("3", "Orientation of an image");
+            ThisMenu.Exercises.Add("4", "Speed camera");
+            ThisMenu.Exercises.Add("x", "Exit program");
+            ThisMenu.DisplayMenu();
 
-            foreach (var exercise in exercises)
-            {
-                Console.WriteLine(exercise.Key + ". " + exercise.Value);
-            }
-
+            // Main Program Loop.
             while (true)
             {
-                var choice = Console.ReadKey(true);
-                int value;
-                if (int.TryParse(choice.KeyChar.ToString(), out value) && exercises.ContainsKey(value))
+                var choice = Console.ReadKey(true).KeyChar.ToString().ToLower();
+                if (ThisMenu.Exercises.ContainsKey(choice))
                 {
-                    var methodName = "Exercise" + value;
+                    var methodName = "Exercise" + choice.ToUpper();
                     Type type = typeof(Program);
                     MethodInfo methodToCall = type.GetMethod(methodName);
                     if (methodToCall != null) methodToCall.Invoke(methodName, null);
                 }
             }
+        }
 
+        public static bool RunAgain()
+        {
+            Console.Write("\nPress 'r' to run again or 'm' to return to menu => ");
+            while (true)
+            {
+                var choice = Console.ReadKey(true).KeyChar.ToString().ToLower();
+                if (choice == "r")
+                {
+                    Console.WriteLine("r\n");
+                    return true;
+                }
+                if (choice == "m")
+                {
+                    return false;
+                }
+            }
         }
 
         public static void Exercise1()
         {
-            Console.WriteLine("Number between 1 and 10");
+            Console.Clear();
+
+            var runAgain = true;
+            while (runAgain)
+            {
+                Console.WriteLine("Exercise 1: Number between 1 and 10\n");
+                Console.Write("Please enter a number between 1 and 10 => ");
+                var left = Console.CursorLeft;
+                var top = Console.CursorTop;
+                var input = Console.ReadLine();
+                if (int.TryParse(input, out var value) && value > 0 && value < 11)
+                {
+                    Console.SetCursorPosition(left + input.Length, top);
+                    Console.WriteLine(" = Valid");
+                }
+                else
+                {
+                    Console.SetCursorPosition(left + input.Length, top);
+                    Console.WriteLine(" = Invalid");
+                }
+                runAgain = RunAgain();
+            }
+            ThisMenu.DisplayMenu();
         }
 
         public static void Exercise2()
         {
-            Console.WriteLine("Maximum of two numbers");
+            Console.Clear();
+            var runAgain = true;
+            while (runAgain)
+            {
+                Console.WriteLine("Exercise 2: Maximum of two numbers\n");
+                var userNumbers = new int[2];
+                var number = 0;
+                bool validChoice = false;
+                while (number < 2)
+                {
+                    while (!validChoice)
+                    {
+                        var text = number == 0 ? "first" : "second";
+                        Console.Write($"Please enter your {text} number => ");
+                        var left = Console.CursorLeft;
+                        var top = Console.CursorTop;
+                        var input = Console.ReadLine();
+                        if (int.TryParse(input, out var value))
+                        {
+                            userNumbers[number] = value;
+                            validChoice = true;
+                        }
+                        else
+                        {
+                            Console.SetCursorPosition(left + input.Length, top);
+                            Console.WriteLine(" = Invalid");
+                        }
+                    }
+                    number++;
+                    validChoice = false;
+                }
+
+                Console.WriteLine($"The maximum number is: {userNumbers.Max()}");
+
+                runAgain = RunAgain();
+            }
+            ThisMenu.DisplayMenu();
         }
 
         public static void Exercise3()
         {
-            Console.WriteLine("Orientation of an image");
+            Console.Clear();
+            var runAgain = true;
+            while (runAgain)
+            {
+                Console.WriteLine("Exercise 3: Orientation of an image\n");
+                string text;
+                var userNumbers = new int[2];
+                var number = 0;
+                bool validChoice = false;
+                while (number < 2)
+                {
+                    while (!validChoice)
+                    {
+                        text = number == 0 ? "width" : "height";
+                        Console.Write($"Please enter the {text} of the image => ");
+                        var left = Console.CursorLeft;
+                        var top = Console.CursorTop;
+                        var input = Console.ReadLine();
+                        if (int.TryParse(input, out var value))
+                        {
+                            userNumbers[number] = value;
+                            validChoice = true;
+                        }
+                        else
+                        {
+                            Console.SetCursorPosition(left + input.Length, top);
+                            Console.WriteLine(" = Invalid");
+                        }
+                    }
+                    number++;
+                    validChoice = false;
+                }
+
+                text = userNumbers[0] > userNumbers[1] ? "landscape" : "portrait";
+                if (userNumbers[0] == userNumbers[1]) text = "square";
+                Console.WriteLine($"The image is: {text}");
+
+                runAgain = RunAgain();
+            }
+            ThisMenu.DisplayMenu();
         }
 
         public static void Exercise4()
         {
-            Console.WriteLine("Speed camera");
+            Console.Clear();
+            var runAgain = true;
+            while (runAgain)
+            {
+                Console.WriteLine("Exercise 4: Speed Camera\n");
+                string text;
+                var userNumbers = new int[2];
+                var number = 0;
+                bool validChoice = false;
+                while (number < 2)
+                {
+                    while (!validChoice)
+                    {
+                        text = number == 0 ? "speed limit" : "speed of the car";
+                        Console.Write($"Please enter the {text} => ");
+                        var left = Console.CursorLeft;
+                        var top = Console.CursorTop;
+                        var input = Console.ReadLine();
+                        if (int.TryParse(input, out var value))
+                        {
+                            userNumbers[number] = value;
+                            validChoice = true;
+                        }
+                        else
+                        {
+                            Console.SetCursorPosition(left + input.Length, top);
+                            Console.WriteLine(" = Invalid");
+                        }
+                    }
+                    number++;
+                    validChoice = false;
+                }
+
+                int demeritPoints = Math.Max(0, userNumbers[1] - userNumbers[0] + 4) / 5;
+
+                text = userNumbers[0] >= userNumbers[1] ? "OK" : demeritPoints + " demerit points";
+                text += demeritPoints > 12 ? " - license suspended" : "";
+                Console.WriteLine($"Speed camera result: {text}");
+
+                runAgain = RunAgain();
+            }
+            ThisMenu.DisplayMenu();
         }
+
+        public static void ExerciseX()
+        {
+            Console.Write("\nThank you for using this program. ");
+            Environment.Exit(0);
+        }
+
     }
 
 }
---
+++ ProgramMenu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercisesConditionals
+{
+    public class ProgramMenu
+    {
+        public string Title { get; set; }
+
+        public Dictionary<string, string> Exercises = new Dictionary<string, string>();
+
+        public void DisplayMenu()
+        {
+            Console.Clear();
+            Console.WriteLine(Title);
+            foreach (var exercise in Exercises)
+            {
+                Console.WriteLine(exercise.Key + ". " + exercise.Value);
+            }
+        }
+
+    }
+}
